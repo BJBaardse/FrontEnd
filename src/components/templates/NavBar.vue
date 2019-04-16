4c143c3eca290fb6e37dc9e443eb0a6ff057ca2d
BJBaardse <beer.baardse@gmail.com>
2019-04-16 12:23:20 +0200

    NavBar language support

--- src/components/templates/NavBar.vue
+++ src/components/templates/NavBar.vue
@@ -1,33 +1,33 @@
 <template>
-<div>
-  <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-navbar-brand v-bind:to="'Login'">Monaco</b-navbar-brand>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand v-bind:to="'Login'">Monaco</b-navbar-brand>
 
-    <b-navbar-toggle target="nav_collapse" />
+      <b-navbar-toggle target="nav_collapse" />
 
-    <b-collapse is-nav id="nav_collapse">
-      <b-navbar-nav>
-        <b-nav-item v-bind:to="'mijnautos'">
-          Mijn auto's
-        </b-nav-item>
-      </b-navbar-nav>
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav>
+          <b-nav-item v-bind:to="'mijnautos'">
+            Mijn auto's
+          </b-nav-item>
+        </b-navbar-nav>
 
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-        <b-nav-item-dropdown text="Taal" right v-model="$i18n.locale">
-          <b-dropdown-item v-for="(lang, i) in langs" :key="`Lang${i}`" :value="lang" v-on:click="SwitchLang(lang)">{{ lang }}</b-dropdown-item>
-        </b-nav-item-dropdown>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown text="Taal" right v-model="$i18n.locale">
+            <b-dropdown-item v-for="(lang, i) in langs" :key="`Lang${i}`" :value="lang" v-on:click="SwitchLang(lang)">{{ lang }}</b-dropdown-item>
+          </b-nav-item-dropdown>
 
-        <b-nav-item-dropdown right>
-          <!-- Using button-content slot -->
-          <template slot="button-content"><em>Gebruiker</em></template>
-          <b-dropdown-item v-bind:to="'profile'">Settings</b-dropdown-item>
-          <b-dropdown-item v-on:click="Logout">Uitloggen</b-dropdown-item>
-        </b-nav-item-dropdown>
-      </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
-</div>
+          <b-nav-item-dropdown right>
+            <!-- Using button-content slot -->
+            <template slot="button-content"><em>Gebruiker</em></template>
+            <b-dropdown-item v-bind:to="'profile'">Settings</b-dropdown-item>
+            <b-dropdown-item v-on:click="Logout">Uitloggen</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
 </template>
 <script>
   import 'bootstrap/dist/css/bootstrap.css'
