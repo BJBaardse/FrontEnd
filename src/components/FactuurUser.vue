e3dc80d4bcc8bd2568d9298defc58b3725d259f0
BJBaardse <beer.baardse@gmail.com>
2019-06-05 12:32:04 +0200

    Router update

--- src/components/FactuurUser.vue
+++ src/components/FactuurUser.vue
@@ -97,7 +97,7 @@
       billDetails: function(){
         if(this.model !== null) {
           sessionStorage.setItem("selectedfactuur", JSON.stringify(this.model.rides));
-          this.$router.push('/ritten');
+          this.$router.push('/factuur_user/ritten');
         }
       }
 
